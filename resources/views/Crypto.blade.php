f98d989b8c517eae2ee163fa298f2861488763b5
Arun <karunsabari@gmail.com>
2023-04-19 18:55:51 +0530

    payment CCA20

--- resources/views/Crypto.blade.php
+++ resources/views/Crypto.blade.php
@@ -7,7 +7,7 @@
 */
 function encrypts($plainText,$key)
 {
-	$key = hextobin(md5($key));
+	$key = hextobins(md5($key));
 	$initVector = pack("C*", 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f);
 	$openMode = openssl_encrypt($plainText, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $initVector);
 	$encryptedText = bin2hex($openMode);
@@ -21,9 +21,9 @@ function encrypts($plainText,$key)
 */
 function decrypts($encryptedText,$key)
 {
-	$key = hextobin(md5($key));
+	$key = hextobins(md5($key));
 	$initVector = pack("C*", 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f);
-	$encryptedText = hextobin($encryptedText);
+	$encryptedText = hextobins($encryptedText);
 	$decryptedText = openssl_decrypt($encryptedText, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $initVector);
 	return $decryptedText;
 }
