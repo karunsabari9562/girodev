2a78d2eb47419ea114301bc7259a28c4978ba493
Arun <karunsabari@gmail.com>
2023-04-19 15:24:00 +0530

    payment CCA4

--- resources/views/ccavRequestHandler.blade.php
+++ resources/views/ccavRequestHandler.blade.php
@@ -8,7 +8,7 @@
 
 <?php 
 
-	error_reporting(0);
+	//error_reporting(0);
 	
 	$merchant_data='2288915';
 	$working_key='AVUO48KD41AU46OUUA';//Shared by CCAVENUES
@@ -21,7 +21,7 @@
 	$encrypted_data=encrypt($merchant_data,$working_key); // Method for encrypting the data.
 
 ?>
-<form method="post" name="redirect" action="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
+<form method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
 <?php
 echo "<input type=hidden name=encRequest value=$encrypted_data>";
 echo "<input type=hidden name=access_code value=$access_code>";
