fb2885ba348f2e08163c1d8e289dddd6f70645ec
Arun <karunsabari@gmail.com>
2023-04-19 12:46:10 +0530

    payment CCA

---
+++ app/Http/Controllers/OnlinePayController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+
+class OnlinePayController extends Controller
+{
+
+public function create()
+    {
+     
+      return view('pay');
+
+    }
+
+    public function payhand()
+    {
+     
+      return view('ccavRequestHandler');
+
+    }
+	
+
+
+}
+
