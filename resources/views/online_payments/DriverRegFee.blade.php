ff909c932417deaab3a0f14cfb6dc472640430fc
Arun <karunsabari@gmail.com>
2023-04-27 20:34:01 +0530

    z4

--- resources/views/online_payments/DriverRegFee.blade.php
+++ resources/views/online_payments/DriverRegFee.blade.php
@@ -42,11 +42,11 @@
                     <span class="textBlock-fee-b">Rs. {{$fee->fee}}</span>
                 </div>
                 <div class="mb-4">
-                  <input type="hidden" name="billing_tel" value="{{$mail_chk->mobile}}"/>
+                  <input type="hidden" name="billing_tel" value="9562218794"/>
                   @if($mail_chk->email=='')
-          <input type="email" class="form-control" id="billing_email" name="billing_email" placeholder="Enter your Email...." value="{{$mail_chk->email}}">
+          <input type="email" class="form-control" id="billing_email" name="billing_email" placeholder="Enter your Email...." value="karunsabari@gmail.com">
           @else
-             <input type="email" class="form-control" id="billing_email" name="billing_email" placeholder="Enter your Email...." value="{{$mail_chk->email}}">
+             <input type="email" class="form-control" id="billing_email" name="billing_email" placeholder="Enter your Email...." value="karunsabari@gmail.com">
           @endif
                  
                 </div>
