a1dcd65ad37d8e29ce64188471c8dbb5e17ceb03
Arun <karunsabari@gmail.com>
2023-08-17 14:39:52 +0530

    aq3

--- app/Http/Controllers/rides/CustomerRideBookings.php
+++ app/Http/Controllers/rides/CustomerRideBookings.php
@@ -104,7 +104,7 @@ class CustomerRideBookings extends Controller
                 if(!$drivers->isEmpty())
                 foreach ($drivers as $d)
                  {
-                  if($d->distances<=50)
+                  if($d->distances<=5)
                   {
                     $available_drivers=count($drivers);
                     $dr[]= $d;
